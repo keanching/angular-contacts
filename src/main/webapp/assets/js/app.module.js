19b7fea2617af7532dd167cf11e8894fae5dc514
Kean Ching <kching25@gmail.com>
2020-05-16 20:30:14 -0400

    Removing webapp dir

--- src/main/webapp/assets/js/app.module.js
+++
@@ -1,7 +0,0 @@
-// Declare the main module and dependent modules
-(function() {
-    "use strict";
-    
-    angular.module("ngContacts", ["ngMessages", "angular-logger", "angular-growl", "ui.router", "ct.ui.router.extras"]);
-    
-})();
